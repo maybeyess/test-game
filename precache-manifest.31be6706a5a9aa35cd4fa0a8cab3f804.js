6016e7c57757fcf7b9b93512463a4ed47b80c131
egor <segor94@bk.ru>
2020-06-19 20:06:21 +0300

    Updates

---
+++ precache-manifest.31be6706a5a9aa35cd4fa0a8cab3f804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6605d35530fdc01807359566ec3fa6e3",
+    "url": "/test-game/index.html"
+  },
+  {
+    "revision": "1d92cedc939e0db07307",
+    "url": "/test-game/static/css/main.553de615.chunk.css"
+  },
+  {
+    "revision": "6e4d764df1e309aa6ec0",
+    "url": "/test-game/static/js/2.32319968.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-game/static/js/2.32319968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d92cedc939e0db07307",
+    "url": "/test-game/static/js/main.98390a08.chunk.js"
+  },
+  {
+    "revision": "76694811b94ba3b0bd9e",
+    "url": "/test-game/static/js/runtime-main.a8c864ea.js"
+  }
+]);
